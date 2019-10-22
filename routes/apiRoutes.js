7956fa47426203c261dee8ea9b1e7eb778d47bd5
Randall <rfields1128@yahoo.com>
2019-10-22 11:31:45 -0500

    updated styles folder route for the main page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,21 +2,21 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
+  app.get("/api/athletes", function(req, res) {
     db.Athletes.findAll({}).then(function(dbResponse) {
       res.json(dbResponse);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
+  app.post("/api/athletes", function(req, res) {
     db.Athletes.create(req.body).then(function(dbResponse) {
       res.json(dbResponse);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
+  app.delete("/api/athletes/:id", function(req, res) {
     db.Athletes.destroy({ where: { id: req.params.id } }).then(function(
       dbResponse
     ) {
